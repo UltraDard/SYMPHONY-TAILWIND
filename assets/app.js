a23cec75fac564272a060a466c87067365bbae61
UltraDard <maxdoualle685@gmail.com>
2025-03-01 14:05:14 +0100

    fix

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+//Pour le theme Bootstrap
+import '../vendor/phpunit/php-code-coverage/src/Report/Html/Renderer/Template/css/bootstrap.min.css'
+import './bootstrap.js';
+//
+////tailwind
+import './styles/app.css';
+//
+////import un tas d'icon
+//import '@iconify/iconify'; //{{ ux_icon('lucide:*') }}
